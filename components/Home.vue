433b99f4c6e526c6d92751ebd1c644285c2bce45
Andrei Timotin <timotin.andrei@justconsult.md>
2021-12-14 15:11:50 +0200

    search-dossier section (desktop layout) on front-page

--- components/Home.vue
+++ components/Home.vue
@@ -58,6 +58,42 @@
             </div>
           </div>
         </section>
+        <section class="search-dossier">
+          <div class="dossier-block">
+            <form action="">
+              <div class="input-block">
+                <input class="dossier_number bg-gray-200 appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                       type="text" id="dossier_number"
+                       placeholder="Numărul dosarului" name="dossier_number">
+              </div>
+              <div class="get-info-block">
+                <p class="questions">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <p class="questions">Lorem ipsum dolor sit amet.</p>
+                <img class="get-info-image" src="" alt="Link to partners">
+                <button class="get-info-button" type="submit">Află numărul dosarului</button>
+              </div>
+            </form>
+          </div>
+          <div class="order-block">
+            <form action="">
+              <div class="input-block">
+               <input class="dossier_number bg-gray-200 appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                     type="text" placeholder="Numărul ordinului" name="order_number">
+              </div>
+
+              <div class="text-block">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                <p>Lorem ipsum dolor sit.</p>
+              </div>
+              <div class="subscribe-block">
+                <input class="subscribe-block__item" type="text" placeholder="Nume Prenume">
+                <input class="subscribe-block__item" type="tel" placeholder="Număr de contact">
+                <input class="subscribe-block__item" type="email" placeholder="Email">
+                <button class="subscribe-block__item subscribe-button" type="submit">Abonează-te</button>
+              </div>
+            </form>
+          </div>
+        </section>
       </main>
     </div>
   </div>
@@ -162,6 +198,90 @@ section.citizenship-steps {
   font-weight: bold;
 }
 .citizenship-steps__step .text {
+
+}
+section.search-dossier {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 40px;//TODO: delete;
+  max-width: 80%;//TODO: delete
+}
+
+.dossier-block {
+  margin-left: 24px;
+}
+
+.dossier_number {
+
+}
+
+.input-block {
+  background-color: #ecb1b1;
+}
+
+.get-info-block {
+  background-color: #424242;
+  font-size: 22px;
+  color: #fff;
+  text-align: center;
+}
+
+.get-info-block .questions {
+
+}
+
+.get-info-block .get-info-button {
+  color: #000;
+  font-size: 22px;
+  border-radius: 2px;
+  background: #72BB5D;
+}
+
+.get-info-image {
+  min-width: 140px;
+  min-height: 140px;
+}
+
+.order-block {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-left: 49px;
+  background-color: #424242;
+  font-size: 22px;
+  color: #fff;
+  text-align: center;
+}
+
+.text-block {
+}
+
+.subscribe-block {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.client-name {
+}
+
+.subscribe-button {
+  color: #000;
+  font-size: 22px;
+  border-radius: 2px;
+  background: #72BB5D;
+}
+
+.subscribe-block__item {
+  margin-top: 12px;
+}
+input.subscribe-block__item{
+  color: #000;
+}
+input.subscribe-block__item::placeholder{
+  color: #000;
+  font-weight: normal;
 
 }
 </style>
